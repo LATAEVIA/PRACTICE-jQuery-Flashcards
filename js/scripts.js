851e289adea9f60e2dd169e3ae02053bdb90bc1a
Anthony Gold and LaTaevia Berry <anthony@>
2016-03-22 13:35:08 -0700

    adds rows, columns, and panels with color, headings, and bodies

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-  $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
-  });
-
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
-
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-  });
-  $("button#white").click(function() {
-    $("body").removeClass();
-    $("body").addClass("revert-colors");
-  });
-  $("button#blue").click(function() {
-    // $("body").removeClass();
-    $("#blue-text-border").addClass("add-border");
-  });
-});
+// $(document).ready(function() {
+//   $("button#green").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("green-background");
+//   });
+//
+//   $("button#yellow").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("yellow-background");
+//   });
+//
+//   $("button#red").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("red-background");
+//   });
+//   $("button#white").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("revert-colors");
+//   });
+//   $("button#blue").click(function() {
+//     // $("body").removeClass();
+//     $("#blue-text-border").addClass("add-border");
+//   });
+// });
